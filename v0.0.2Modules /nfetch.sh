4b5c0cdaf67c617c2c6ecbb70a56fcb956376eb1
Brian Mutune <brianxborne@gmail.com>
2025-07-16 16:40:18 +0300

    Update nfetch.sh

--- v0.0.2Modules /nfetch.sh
+++ v0.0.2Modules /nfetch.sh
@@ -18,5 +18,6 @@ echo "Fetching bridge.log and name.sh into $root_dir"
 cd "$root_dir" || exit
 wget -O bridge.log https://raw.githubusercontent.com/Neural-Agent-Modelling-Engine/Scripts/main/v0.0.2/bridge.log
 wget -O name.sh   https://raw.githubusercontent.com/Neural-Agent-Modelling-Engine/Scripts/main/v0.0.2/name.sh
+chmod +x name.sh
 
 echo "Fetch complete."
